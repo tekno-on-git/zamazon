cc5fa718ff84bdab2d43fab24a10cdc71095f0af
codeph-0bia <codephobia642@gmail.com>
2023-12-11 00:06:25 +0530

    ADD add-product page

---
+++ src/app/add-product/page.tsx
@@ -0,0 +1,64 @@
+import FormSubmitButton from "@/components/FormSubmitButton";
+import { prisma } from "@/lib/db/prisma";
+import { redirect } from "next/navigation";
+
+export const metadata = {
+  title: "Add product | Zamazon",
+};
+
+async function addProduct(formData: FormData) {
+  "use server";
+
+  const name = formData.get("name")?.toString();
+  const description = formData.get("description")?.toString();
+  const imageUrl = formData.get("imageUrl")?.toString();
+  const price = Number(formData.get("price") || 0);
+  //   throw Error("WOAH");
+
+  if (!name || !description || !imageUrl || !price) {
+    throw Error("Missing required fields");
+  }
+  await prisma.product.create({
+    data: { name, description, imageUrl, price },
+  });
+
+  redirect("/");
+}
+
+export default function AddProductPage() {
+  return (
+    <div className="mb-3 text-lg font-bold">
+      <h1>Add Product</h1>
+      <form action={addProduct}>
+        <input
+          required
+          type="text"
+          name="name"
+          placeholder="Name"
+          className="input input-bordered mb-3 w-full"
+        />
+        <textarea
+          required
+          name="description"
+          placeholder="Description"
+          className="textarea textarea-bordered mb-3 w-full"
+        />
+        <input
+          type="url"
+          required
+          name="imageUrl"
+          placeholder="Image URL"
+          className="input input-bordered mb-3 w-full"
+        />
+        <input
+          required
+          type="number"
+          name="price"
+          placeholder="Price"
+          className="input input-bordered mb-3 w-full"
+        />
+        <FormSubmitButton className=" btn-block">Add Product</FormSubmitButton>
+      </form>
+    </div>
+  );
+}
